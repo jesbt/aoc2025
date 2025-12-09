24bbb6dce438c84a5485d0d8e79a3cfb75a4bcbd
Jesus Bautista <jesbt4@gmail.com>
2025-12-09 15:25:42 -0600

    Add solution for day 2 - part 2

--- day2/main.go
+++ day2/main.go
@@ -14,6 +14,18 @@ func check(e error) {
 	}
 }
 
+func getValidDividers(number int) []int {
+	dividers := []int{}
+
+	for i := 1; i <= number/2; i++ {
+		if number%i == 0 {
+			dividers = append(dividers, i)
+		}
+	}
+
+	return dividers
+}
+
 func partOne(filePath string) {
 	file, err := os.ReadFile(filePath)
 	check(err)
@@ -53,10 +65,47 @@ func partOne(filePath string) {
 	fmt.Println(invalidIds)
 }
 
+func partTwo(filePath string) {
+	file, err := os.ReadFile(filePath)
+	check(err)
+
+	idRanges := strings.Split(string(file), ",")
+
+	invalidIds := 0
+
+	for _, row := range idRanges {
+		idRange := strings.Split(row, "-")
+		if len(idRange) != 2 {
+			log.Println("Skipping invalid range: ", row)
+			continue
+		}
+
+		start, err := strconv.Atoi(idRange[0])
+		check(err)
+		end, err := strconv.Atoi(idRange[1])
+		check(err)
+
+		for i := start; i <= end; i++ {
+			id := strconv.Itoa(i)
+			validDividers := getValidDividers(len(id))
+
+			for _, div := range validDividers {
+				if strings.Repeat(id[:div], len(id)/div) == id {
+					invalidIds += i
+					break
+				}
+			}
+		}
+
+	}
+	fmt.Println(invalidIds)
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: main ./input.txt")
 	}
 
 	partOne(os.Args[1])
+	partTwo(os.Args[1])
 }
